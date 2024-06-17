cc989b56094659d757c778ee3a59a94df7420d0a
Javier Diez <javierdiez92@gmail.com>
2024-06-17 12:10:17 +0000

    delete all tasks

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -8,7 +8,7 @@ export const ToDoList = () => {
     const [listadoTareas, setListadoTareas] = useState([])
 
 
-    const handleChange = (e) =>{
+    const handleChange = (e) => {
         setTareaNueva(e.target.value)
     }
 
@@ -21,7 +21,7 @@ export const ToDoList = () => {
         }
     };
 
-    
+
 
 
     const handleDelete = async (id) => {
@@ -29,133 +29,147 @@ export const ToDoList = () => {
         fetchGetTask();
     }
 
-        const deletAllTasks = () => {
-            setListadoTareas([])
-        }
+    const handleDeleteAllTasks = async (id) => {
+        await fetchDeleteAllTasks(id);
+        fetchGetTask();
+    }
 
-        const fetchCreateUser = () => {
-            fetch('https://playground.4geeks.com/todo/users/jdiezolivari', {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json"
-                }
-              })
-              .then(resp => {
-                  console.log(resp.ok); // Será true si la respuesta es exitosa
-                  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-                  return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-              })
-              .then(data => {
-                    console.log(data);
-              
+
+    const fetchCreateUser = async () => {
+        fetch('https://playground.4geeks.com/todo/users/jdiezolivari', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => {
+                console.log(resp.ok); // Será true si la respuesta es exitosa
+                console.log(resp.status); // El código de estado 200, 300, 400, etc.
+                return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
             })
-              .catch(error => {
-                  // Manejo de errores
-                  console.log(error);
-              });
+            .then(data => {
+                console.log(data);
+
+            })
+            .catch(error => {
+                // Manejo de errores
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchCreateUser();
+    }, [])
+
+    const todos = { label: tareaNueva, is_done: false }
+    const fetchPutTask = () => {
+        fetch('https://playground.4geeks.com/todo/todos/jdiezolivari', {
+            method: "POST",
+            body: JSON.stringify(todos),
+            headers: {
+                "Content-Type": "application/json"
             }
-
-            useEffect(() => {
-                fetchCreateUser(); 
-            },[])
-
-            const todos = {label : tareaNueva, is_done: false}
-            const fetchPutTask = () => {
-            fetch('https://playground.4geeks.com/todo/todos/jdiezolivari', {
-                method: "POST",
-                body: JSON.stringify(todos),
-                headers: {
-                  "Content-Type": "application/json"
-                }
-              })
-              .then(resp => {
-                  console.log(resp.ok); // Será true si la respuesta es exitosa
-                  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-                  return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-              })
-              .then(data => {
-                  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-                  fetchGetTask();
-
-    
-              })
-              .catch(error => {
-                  // Manejo de errores
-                  console.log(error);
-              });
-            }
+        })
+            .then(resp => {
+                console.log(resp.ok); // Será true si la respuesta es exitosa
+                console.log(resp.status); // El código de estado 200, 300, 400, etc.
+                return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+            })
+            .then(data => {
+                console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+                fetchGetTask();
 
 
-            const fetchGetTask = () => {
-                fetch('https://playground.4geeks.com/todo/users/jdiezolivari', {
-                    method: "GET",
-                  })
-                  .then(resp => {
-                      console.log(resp.ok); // Será true si la respuesta es exitosa
-                      console.log(resp.status); // El código de estado 200, 300, 400, etc.
-                      return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-                  })
-                  .then(data => {
-                      console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-                      setListadoTareas(data.todos)
-                      
-                  })
-                  .catch(error => {
-                      // Manejo de errores
-                      console.log(error);
-                  });
-                }
-        
-                useEffect(() => {
-                    fetchGetTask();
-                },[])
+            })
+            .catch(error => {
+                // Manejo de errores
+                console.log(error);
+            });
+    }
 
-            
-        
 
-        const fetchDeletetTask = (id) => {
-            fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+    const fetchGetTask = () => {
+        fetch('https://playground.4geeks.com/todo/users/jdiezolivari', {
+            method: "GET",
+        })
+            .then(resp => {
+                console.log(resp.ok); // Será true si la respuesta es exitosa
+                console.log(resp.status); // El código de estado 200, 300, 400, etc.
+                return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+            })
+            .then(data => {
+                console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+                setListadoTareas(data.todos)
+
+            })
+            .catch(error => {
+                // Manejo de errores
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchGetTask();
+    }, [])
+
+
+
+    const fetchDeletetTask = async (id) => {
+        try {
+            const resp = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
                 method: "DELETE",
                 headers: {
-                  "Content-Type": "application/json"
+                    "Content-Type": "application/json"
                 }
-              })
-              .then(resp => {
-                  console.log(resp.ok); // Será true si la respuesta es exitosa
-                  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-                  return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-              })
-              .then(resp => {
-                if (resp.ok) {
-                    return resp.text(); // Usa text() en lugar de json()
-                }
-                throw new Error('Failed to delete task');
-            })
-              .catch(error => {
-                  // Manejo de errores
-                  console.log(error);
-              });
+            });
+            if (resp.ok) {
+                return await resp.text(); // Usa text() en lugar de json()
             }
+            throw new Error('Failed to delete task');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchDeleteAllTasks = async () => {
+        try {
+            const resp = await fetch(`https://playground.4geeks.com/todo/users/jdiezolivari`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            if (!resp.ok) {
+				throw new Error("Error deleting all ToDos");
+			}
+
+			await fetchCreateUser();
+			setListadoTareas([]);
+			console.log("Tareas eliminadas");
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 
-   return ( 
-    <>
-    
-   <div className="d-flex justify-content-center mx-auto align-items-center flex-column">
-   <h2 className="text-light display-2 mt-5">To Do List</h2>
-    
-    <div className="contenedor-tareas">
-        <input className="input-tarea" type="text" onChange={handleChange} onKeyDown={handleKeyDown} value={tareaNueva}/>
+    return (
+        <>
 
-        <div className="text-start p-3 fs-3">{listadoTareas.map((tarea, index) =><ul key={tarea.id} className="listado-tareas d-flex justify-content-between border-bottom align-items-center"> <li className="w-100">{tarea.label}</li><span className="cruz fs-1 fw-bold" onClick={() => handleDelete(tarea.id)}>x</span></ul>)}</div>
+            <div className="d-flex justify-content-center mx-auto align-items-center flex-column">
+                <h2 className="text-light display-2 mt-5">To Do List</h2>
+
+                <div className="contenedor-tareas">
+                    <input className="input-tarea" type="text" onChange={handleChange} onKeyDown={handleKeyDown} value={tareaNueva} />
+
+                    <div className="text-start p-3 fs-3">{listadoTareas.map((tarea, index) => <ul key={tarea.id} className="listado-tareas d-flex justify-content-between border-bottom align-items-center"> <li className="w-100">{tarea.label}</li><span className="cruz fs-1 fw-bold" onClick={() => handleDelete(tarea.id)}>x</span></ul>)}</div>
 
 
-        {/* <div className="text-start p-3 fs-3">{listadoTareas.map((tarea, index) =><ul key={index} className="listado-tareas d-flex justify-content-between border-bottom align-items-center"> <li className="w-100">{tarea}</li><span className="cruz fs-1 fw-bold" onClick={() => handleDelete(index)}>x</span></ul>)}</div> */}
-        <p className="fs-3 text-primary">{listadoTareas.length > 0 ? `Tareas pendientes: ${listadoTareas.length}` : 'No hay tareas pendientes'}</p>
-        <button className="btn btn-danger mb-5" onClick={deletAllTasks}>ELIMINAR TODAS LAS TAREAS</button>
-    </div>
+                    {/* <div className="text-start p-3 fs-3">{listadoTareas.map((tarea, index) =><ul key={index} className="listado-tareas d-flex justify-content-between border-bottom align-items-center"> <li className="w-100">{tarea}</li><span className="cruz fs-1 fw-bold" onClick={() => handleDelete(index)}>x</span></ul>)}</div> */}
+                    <p className="fs-3 text-primary">{listadoTareas.length > 0 ? `Tareas pendientes: ${listadoTareas.length}` : 'No hay tareas pendientes'}</p>
+                    <button className="btn btn-danger mb-5" onClick={handleDeleteAllTasks}>ELIMINAR TODAS LAS TAREAS</button>
+                </div>
 
-    </div>
-    </>
-   )
+            </div>
+        </>
+    )
 }
